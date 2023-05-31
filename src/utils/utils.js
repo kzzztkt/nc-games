6a9edf760d6e98f7bb23f20da25a93a75e858f86
benvhshed <47852378+benvstars@users.noreply.github.com>
2023-05-31 15:51:34 +0100

    added await functions

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,36 +4,32 @@ const mainApi = axios.create({
     baseURL: "https://bojosgameshop.onrender.com/api"
 })
 
-function getReviews (){
-    return mainApi
-    .get(`./reviews`)
-    .then((res) =>{
-        return res.data.reviews;
-    })
-    .catch((err) =>{
+async function getReviews (){
+    try {
+        const res = await mainApi.get(`./reviews`)
+            return res.data.reviews;
+    } catch (err) {
         console.log(err);
-    })
+    }
 }
-function getReviewById (id){
-    return mainApi
-    .get(`./reviews/${id}`)
-    .then((res) =>{
-        return res.data;
-    })
-    .catch((err) =>{
+async function getReviewById (id){
+    try {
+        const res = await mainApi.get(`./reviews/${id}`)
+            return res.data;
+    } catch (err) {
         console.log(err);
-    })
+    }
 }
 
-function getComments (id){
-    return mainApi
-    .get(`./reviews/${id}/comments`)
-    .then((res) =>{
-        return res.data;
-    })
-    .catch((err) =>{
+async function getComments (id){
+    try {
+        const res = await mainApi.get(`./reviews/${id}/comments`)
+            return res.data;
+    } catch (err) {
         console.log(err);
-    })
+    }
 }
+
+
 
 export {getReviews, getReviewById, getComments};
